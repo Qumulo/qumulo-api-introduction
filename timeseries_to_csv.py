524290832669f8af738712ac5bef0501297e369b
Carson Boden <cwboden@umich.edu>
2021-05-04 14:09:03 -0700

    Fix formatting, use f-strings

--- timeseries_to_csv.py
+++ timeseries_to_csv.py
@@ -35,15 +35,15 @@ def main():
     rc.login(args.user, args.passwd);
 
     last_line = None
-    columns = ["iops.read.rate", "iops.write.rate", 
-               "throughput.read.rate", "throughput.write.rate",
-               "reclaim.deferred.rate", "reclaim.snapshot.rate"]
+    columns = ['iops.read.rate', 'iops.write.rate',
+               'throughput.read.rate', 'throughput.write.rate',
+               'reclaim.deferred.rate', 'reclaim.snapshot.rate']
 
-    csv_file_name = "qumulo-timeseries-data.csv"
+    csv_file_name = 'qumulo-timeseries-data.csv'
 
     if os.path.exists(csv_file_name):
         # read to the last line in the file
-        with open(csv_file_name, "rb") as csvfile:
+        with open(csv_file_name, 'rb') as csvfile:
             reader = csv.reader(csvfile)
             for row in reader:
                 last_line = row
@@ -51,10 +51,11 @@ def main():
     # at most we'll have 1 day of data
     begin_time = int(time.time()) - 60 * 60 * 24
     # otherwise, pull only the latest data, if we already have a file
-    if last_line is not None: 
+    if last_line is not None:
         begin_time = int(last_line[0]) + 5
     results = rc.analytics.time_series_get(begin_time = begin_time)
-    print("Appending %s results to '%s'." % (len(results[0]['values']), csv_file_name))
+    num_results = len(results[0]['values'])
+    print(f'Appending {num_results} results to "{csv_file_name}".')
     data = {}
     for i in range(0,len(results[0]['times'])-1):
         ts = results[0]['times'][i]
@@ -67,12 +68,12 @@ def main():
             ts = series['times'][i]
             data[ts][columns.index(series['id'])] = series['values'][i]
 
-    fw = open(csv_file_name, "a")
+    fw = open(csv_file_name, 'a')
     if last_line is None:
-        fw.write("unix.timestamp,gmtime," + ",".join(columns) + "\r\n")
+        fw.write('unix.timestamp,gmtime,' + ','.join(columns) + '\r\n')
     for ts in sorted(data):
-        gmt = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime(ts))
-        fw.write("%s,%s," % (ts, gmt) + ",".join([str(d) for d in data[ts]]) + "\r\n")
+        gmt = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(ts))
+        fw.write(f'{ts},{gmt},' + ','.join([str(d) for d in data[ts]]) + '\r\n')
     fw.close()
 
 
