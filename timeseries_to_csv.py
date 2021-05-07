23623a77f970e8f7795d8a8310cd752311317ea3
Carson Boden <carson.boden@gmail.com>
2021-05-07 14:16:47 -0700

    Refactor code and add Python tests (#6)
    * Add a requirements.txt to the repo
    
    * Rename Python files to be in snakecase
    
    * Fix formatting, use f-strings
    
    * Extract parse_args() into its own function
    
    * Add unit tests for arg parsing
    
    Also introduce `port` argument for clusters that don't use 8000 as the
    REST port.
    
    * Add the default output filename to the .gitignore
    
    * Extract helpers out of main and add unit tests
    
    * Write integration tests using a mocked response from the cluster
    
    * Fix more formatting, remove unused imports
    
    * Remove duplicate test
    
    * Add MyPy annotations to all test functions
    
    * Fix a few more formatting issues
    
    * Add missed import -- not sure how this missed flake

---
+++ timeseries_to_csv.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# Copyright (c) 2017 Qumulo, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+import argparse
+import csv
+import os
+import time
+import sys
+
+from typing import Any, Mapping, List, Sequence
+
+from qumulo.rest_client import RestClient
+
+
+CSV_FILENAME = 'qumulo-timeseries-data.csv'
+COLUMNS_TO_PROCESS = [
+    'iops.read.rate',
+    'iops.write.rate',
+    'throughput.read.rate',
+    'throughput.write.rate',
+    'reclaim.deferred.rate',
+    'reclaim.snapshot.rate'
+]
+
+
+def parse_args(args: Sequence[str]) -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='Get time series data from a Qumulo cluster, write to CSV',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        'host',
+        help='Qumulo Cluster to communicate with'
+    )
+    parser.add_argument(
+        '-u',
+        '--user',
+        default='admin',
+        help='Username for authentication'
+    )
+    parser.add_argument(
+        '-p',
+        '--password',
+        default='admin',
+        help='Password for authentication'
+    )
+    parser.add_argument(
+        '-P',
+        '--port',
+        type=int,
+        default=8000,
+        help='REST Port for communicating with the cluster'
+    )
+    return parser.parse_args(args)
+
+
+def calculate_begin_time(csv_file_name: str) -> int:
+    """
+    At most, we'll grab 1 day of data, but if we already have some data
+    present, we can just request data since then.
+    """
+    last_line = None
+    if os.path.exists(csv_file_name):
+        # read to the last line in the file
+        with open(csv_file_name, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                last_line = row
+
+    if last_line is not None:
+        return int(last_line[0]) + 5
+    return int(time.time()) - 60 * 60 * 24
+
+
+def read_time_series_from_cluster(
+    host: str,
+    user: str,
+    password: str,
+    port: int
+) -> List[Mapping[str, Any]]:
+    """
+    Communicates with the cluster to grab the analytics in time series format
+    """
+    rest_client = RestClient(host, port)
+    rest_client.login(user, password)
+    return rest_client.analytics.time_series_get(
+            begin_time=calculate_begin_time(CSV_FILENAME))
+
+
+def convert_timeseries_into_dict(
+    results: Sequence[Mapping[str, Any]]
+) -> Mapping[int, Sequence[str]]:
+    """
+    Extracts important values from the timeseries results into a dictionary,
+    keyed by timestamp.
+    """
+
+    if not results:
+        return {}
+
+    # Setup empty lists for each timestamp
+    data = {}
+    for timestamp in results[0]['times']:
+        data[int(timestamp)] = [None] * len(COLUMNS_TO_PROCESS)
+
+    # Extract each data point
+    for series in results:
+        name = series['id']
+        if name not in COLUMNS_TO_PROCESS:
+            continue
+
+        for timestamp, value in zip(series['times'], series['values']):
+            column_idx = COLUMNS_TO_PROCESS.index(name)
+            data[int(timestamp)][column_idx] = value
+
+    return data
+
+
+def write_csv_to_file(
+    data: Mapping[int, Sequence[str]],
+    filename: str
+) -> None:
+    """Write the provided data to the file, creating headers if needed"""
+    with open(filename, 'a') as output_file:
+        # Add headers if they don't exist
+        if os.path.getsize(filename) == 0:
+            columns_csv = ','.join(COLUMNS_TO_PROCESS)
+            output_file.write(f'unix.timestamp,gmtime,{columns_csv}\r\n')
+
+        for ts in sorted(data):
+            gmt = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(ts))
+            data_csv = ','.join([str(d) for d in data[ts]])
+            output_file.write(f'{ts},{gmt},{data_csv}\r\n')
+
+
+def main(sys_args: Sequence[str]):
+    args = parse_args(sys_args)
+    results = read_time_series_from_cluster(
+            args.host, args.user, args.password, args.port)
+    data = convert_timeseries_into_dict(results)
+    write_csv_to_file(data, CSV_FILENAME)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
---
+++ timeseries_to_csv_test.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+import time
+import os
+import unittest
+
+from parameterized import parameterized
+from typing import Mapping, Sequence
+from unittest.mock import patch
+
+from timeseries_to_csv import (
+    COLUMNS_TO_PROCESS,
+    CSV_FILENAME,
+    calculate_begin_time,
+    convert_timeseries_into_dict,
+    main,
+    parse_args,
+    write_csv_to_file,
+)
+
+
+class ArgparseTest(unittest.TestCase):
+    def test_default_arguments(self) -> None:
+        args = parse_args(['my_host'])
+        self.assertEqual(args.host, 'my_host')
+        self.assertEqual(args.user, 'admin')
+        self.assertEqual(args.password, 'admin')
+        self.assertEqual(args.port, 8000)
+
+    @parameterized.expand([['-u'], ['--user']])
+    def test_user(self, user_arg: str) -> None:
+        args = parse_args(['my_host', user_arg, 'my_user'])
+        self.assertEqual(args.user, 'my_user')
+
+    @parameterized.expand([['-p'], ['--password']])
+    def test_password(self, password_arg: str) -> None:
+        args = parse_args(['my_host', password_arg, 'my_user'])
+        self.assertEqual(args.password, 'my_user')
+
+    @parameterized.expand([['-P'], ['--port']])
+    def test_port(self, port_arg: str) -> None:
+        args = parse_args(['my_host', port_arg, '8001'])
+        self.assertEqual(args.port, 8001)
+
+
+def assert_expected_output_file_contents(
+    testcase: unittest.TestCase,
+    filename: str,
+    expected_data: Mapping[int, Sequence[str]]
+) -> None:
+    testcase.assertTrue(os.path.exists(filename))
+    remaining_lines = None
+    with open(filename, 'r') as csv_file:
+        # First line should contain the header
+        testcase.assertIn('unix.timestamp', csv_file.readline())
+        remaining_lines = csv_file.readlines()
+
+    testcase.assertIsNotNone(remaining_lines)
+    testcase.assertEqual(len(expected_data), len(remaining_lines))
+    for data_points, line in zip(expected_data.values(), remaining_lines):
+        for word in data_points:
+            testcase.assertIn(word, line)
+
+
+class HelperTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.filename = 'test-file.csv'
+
+    def tearDown(self) -> None:
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    def test_calculate_begin_time_no_existing_file_uses_last_day(self) -> None:
+        begin_time = calculate_begin_time(self.filename)
+
+        current_time_minus_one_day = int(time.time()) - (60 * 60 * 24)
+        self.assertLessEqual(begin_time, current_time_minus_one_day)
+
+    def test_calculate_begin_time_uses_latest_time_from_existing_file(
+        self
+    ) -> None:
+        with open(self.filename, 'w') as csv_file:
+            csv_file.writelines(['12,\n', '17,\n', '22,\n'])
+
+        begin_time = calculate_begin_time(self.filename)
+        self.assertEqual(begin_time, 27)
+
+    def test_convert_timeseries_into_dict_empty(self) -> None:
+        timeseries = []
+        output = convert_timeseries_into_dict(timeseries)
+
+        # Empty dictionaries evaluate to false
+        self.assertFalse(output)
+
+    def test_convert_timeseries_into_dict_with_no_relevant_data(self) -> None:
+        times = [0, 5, 10]
+        timeseries = [
+            {
+                'times': times,
+                'id': 'foo'
+            },
+            {
+                'times': times,
+                'id': 'bar'
+            },
+            {
+                'times': times,
+                'id': 'baz'
+            }
+        ]
+
+        output = convert_timeseries_into_dict(timeseries)
+
+        # Three timestamps, so should be three rows
+        self.assertEqual(len(output), 3)
+        self.assertEqual(list(output.keys()), times)
+
+        # No relevant data to process, all entries should be empty
+        for row in output.values():
+            self.assertEqual(len(row), len(COLUMNS_TO_PROCESS))
+            for entry in row:
+                self.assertIsNone(entry)
+
+    def test_convert_timeseries_into_dict_with_listed_columns(self) -> None:
+        # N.B. times correspond to the value below
+        times = [0, 5, 10]
+        values = ['foo', 'bar', 'baz']
+
+        timeseries = []
+        for column_id in COLUMNS_TO_PROCESS:
+            timeseries.append({
+                'times': times,
+                'id': column_id,
+                'values': values
+            })
+
+        output = convert_timeseries_into_dict(timeseries)
+
+        for timestamp, row in output.items():
+            expected_value = values[times.index(timestamp)]
+            for entry in row:
+                self.assertEqual(entry, expected_value)
+
+    def test_write_csv_to_file_creates_headers_if_no_file(self) -> None:
+        data = {}
+        write_csv_to_file(data, self.filename)
+
+        self.assertTrue(os.path.exists(self.filename))
+        with open(self.filename, 'r') as csv_file:
+            # First line should contain the header
+            self.assertIn('unix.timestamp', csv_file.readline())
+
+    def test_write_csv_to_file_creates_headers_if_empty_file(self) -> None:
+        data = {}
+
+        # Touch the file, so it exists but is empty
+        with open(self.filename, 'w'):
+            pass
+
+        write_csv_to_file(data, self.filename)
+
+        self.assertTrue(os.path.exists(self.filename))
+        with open(self.filename, 'r') as csv_file:
+            # First line should contain the header
+            self.assertIn('unix.timestamp', csv_file.readline())
+
+    def test_write_csv_to_file_dumps_data_in_csv_format(self) -> None:
+        data = {
+            0: ['foo', 'bar', 'baz'],
+            5: ['fiz', 'bang', 'zap'],
+            10: ['zot', 'buh', 'gul'],
+        }
+
+        write_csv_to_file(data, self.filename)
+        assert_expected_output_file_contents(self, self.filename, data)
+
+    def test_write_csv_to_file_appends_to_existing_file(self) -> None:
+        primary_data = { 0: ['foo', 'bar', 'baz'] }
+        secondary_data = { 5: ['fiz', 'bang', 'zap'] }
+
+        write_csv_to_file(primary_data, self.filename)
+        write_csv_to_file(secondary_data, self.filename)
+
+        combined_data = primary_data
+        combined_data.update(secondary_data)
+        assert_expected_output_file_contents(
+                self, self.filename, combined_data)
+
+
+# N.B. We mock out read_time_series_from_cluster instead of the RestClient
+# since the RestClient has dynamic attributes that are difficult to mock. (i.e.
+# we can't easily mock the call to rest_client.analytics.time_series_get())
+@patch('timeseries_to_csv.read_time_series_from_cluster')
+class IntegrationTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.times = [0, 5, 10]
+        self.values = ['foo', 'bar', 'baz']
+
+        self.timeseries_from_rest_client = []
+        for column_id in COLUMNS_TO_PROCESS:
+            self.timeseries_from_rest_client.append({
+                'times': self.times,
+                'id': column_id,
+                'values': self.values
+            })
+
+        self.expected_data = {
+            0: ['foo'] * len(COLUMNS_TO_PROCESS),
+            5: ['bar'] * len(COLUMNS_TO_PROCESS),
+            10: ['baz'] * len(COLUMNS_TO_PROCESS)
+        }
+
+    def tearDown(self) -> None:
+        if os.path.exists(CSV_FILENAME):
+            os.remove(CSV_FILENAME)
+
+    def test_default_arguments(self, mock_getter) -> None:
+        mock_getter.return_value = self.timeseries_from_rest_client
+        main(['localhost'])
+
+        mock_getter.assert_called_with('localhost', 'admin', 'admin', 8000)
+        assert_expected_output_file_contents(
+                self, CSV_FILENAME, self.expected_data)
+
+    def test_passes_argument_to_rest_client(self, mock_getter) -> None:
+        mock_getter.return_value = self.timeseries_from_rest_client
+        main(['localhost', '-u', 'my_user', '-p', 'my_password', '-P', '8001'])
+
+        mock_getter.assert_called_with(
+                'localhost', 'my_user', 'my_password', 8001)
+        assert_expected_output_file_contents(
+                self, CSV_FILENAME, self.expected_data)
+
+    def test_creates_header_only_file_if_no_data_from_server(
+        self,
+        mock_getter
+    ) -> None:
+        mock_getter.return_value = {}
+        main(['localhost'])
+
+        mock_getter.assert_called_with('localhost', 'admin', 'admin', 8000)
+        assert_expected_output_file_contents(self, CSV_FILENAME, {})
+
+
+if __name__ == '__main__':
+    unittest.main()
